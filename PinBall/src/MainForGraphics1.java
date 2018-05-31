05a5e8c8f99091692138f35f233b3b74d1b00c21
eicing <39416080+eicing@users.noreply.github.com>
2018-05-31 14:04:12 -0400

    Add files via upload

---
+++ PinBall/src/MainForGraphics1.java
@@ -0,0 +1,28 @@
+	import javax.swing.JPanel;
+	import javax.swing.JFrame;
+	public class MainForGraphics1
+	{
+	public static void main(String[] args)
+	{
+
+	JFrame frame = new JFrame();
+
+
+	frame.setSize(1920,1080);
+
+
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+	Panel100 panel = new Panel100();
+	   
+
+	frame.add(panel);
+
+	frame.setVisible(true);
+
+
+	}
+	}
+
+
